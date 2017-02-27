953cc70cce42406236a544395d555f2aaa84d091
chrispk <chrispkennaby@gmail.com>
2017-02-27 23:17:48 +0000

    build visual in html document

--- js/golBoard.js
+++ js/golBoard.js
@@ -1,14 +1,15 @@
-function golBoard(w, h) {
+function GolBoard(w, h) {
 	this.width = w;
 	this.height = h;
 
 	this.boardWrap = true;
+	this.domBoard;
 
 	this.board = [];
-	this.initBoard = function() {
-		for (i = 0;i < w; i++) {
+	this.init = function() {
+		for (i = 0;i < this.width; i++) {
 			this.board[i] = [];
-			for (j = 0;j < y; j++) {
+			for (j = 0;j < this.height; j++) {
 				this.board[i][j] = false;
 			}
 		}
@@ -20,24 +21,34 @@ function golBoard(w, h) {
 	this.updateBoard = function() {
 		this.updateVirtualBoard();
 		this.board = this.virtualBoard;
+		// update visual board -------------------- START HERE
+		for (var i = 0; i < this.width; i++) {
+			for (var j = 0; j < this.height; j++) {
+				if(this.board[i][j]) {
+					var cell = domBoard.childNodes[i].childNodes[j];
+					cell.classList.remove('inactive');
+					cell.classList.add('active');
+				}
+			}
+		}
 	}
 
 	this.makeBoard = function() {
-		var domBoard = document.createElement('div');
+		domBoard = document.createElement('div');
 		domBoard.setAttribute('id', 'golBoard');
+		domBoard.setAttribute('class', 'golboard');
 
-		for (i = 0;i < w;i++) {
+		for (var i = 0; i < this.width; i++) {
 			var column = document.createElement('div');
-			column.setAttribute('class', 'golBoardColumn');
-			domBoard.appendChild(column);
-			for (i = 0;i < w;i++) {
+			column.setAttribute('class', 'golboard-column');
+			for (var j = 0; j < this.height; j++) {
 				var cell = document.createElement('div');
-				cell.setAttribute('class', 'golBoardCell');
-				cell.setAttribute('class', 'inactive');
+				cell.setAttribute('class', 'golboard-cell inactive');
 				column.appendChild(cell);
 			}
+			domBoard.appendChild(column);
 		}
-		document.getElementById('body').appendChild(domBoard);
+		document.getElementsByTagName("BODY")[0].appendChild(domBoard);
 	}
 
 	this.updateVirtualBoard = function() {
@@ -90,3 +101,12 @@ function golBoard(w, h) {
 
 
 }
+
+var golBoard = new GolBoard(10, 10);
+golBoard.init();
+
+golBoard.virtualBoard[4][4] = true;
+golBoard.virtualBoard[4][5] = true;
+golBoard.virtualBoard[5][4] = true;
+golBoard.virtualBoard[5][5] = true;
+
